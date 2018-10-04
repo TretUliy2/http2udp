7f0466e7cb01f303b849a5cd5c182c93e6d3feda
demiurg <demiurg@tica.com.ua>
2018-10-04 08:07:49 +0300

    Usage function added

--- net.go
+++ net.go
@@ -9,100 +9,83 @@ import (
 	"net/http"
 	"os"
 	"syscall"
-	"time"
 )
 
 const (
-	maxdata     int = 131600
-	pktlen      int = 188
-	pkts        int = 7
-	datalen     int = pktlen * pkts
-	defaultport int = 1234
+	bufsize  int = 131600
+	pktsisze int = 188
+	datalen  int = pktsisze * 7
 )
 
-var data []byte
-
 func main() {
-	data = make([]byte, maxdata)
-	sourceurl := flag.String("s", "http://82.193.96.217:12999/udp/239.23.0.5:1234", "a string")
-	destination := flag.String("d", "239.23.15.102", "a string")
-	help := flag.Bool("h", false, "usage")
-	ttl := flag.Int("t", 64, "ttl of destination packets")
-	tos := flag.Int("o", 136, "tos of a packet")
+	source := flag.String("s", "http://10.71.0.190:12999/udp/239.23.0.5:1234",
+		"Source http url")
+	udp := flag.String("d", "239.23.15.102", "destination udp address")
+	ttl := flag.Int("t", 64, "ttl value int default 64")
+	tos := flag.Int("p", 136, "tos value in dec, int default 136")
+	help := flag.Bool("h", false, "help")
+
 	flag.Parse()
-	if *help == true {
+	if *help {
 		usage()
-		os.Exit(0)
 	}
-	fmt.Printf("%d %d\n", *ttl, *tos)
-	readhttp(*sourceurl, *destination, *ttl, *tos)
-}
 
-func readhttp(url string, udpaddr string, ttl int, tos int) {
-	// Function will read http input and assume it have valid mpegts stream
-	// Then it will write to udp multicast destination
-	resp, err := http.Get(url)
-	fmt.Printf("connecting to %s will send output to %s\n", url, udpaddr)
-	for err != nil {
-		log.Printf("Error while connecting to source %v\n", err)
-		time.Sleep(10)
-		resp, err = http.Get(url)
-	}
-	fmt.Println("Dialed successful")
-	defer resp.Body.Close()
-	// Udp Connection open
-	madr := net.UDPAddr{IP: net.ParseIP(udpaddr), Port: defaultport}
-	udpConn, udpConErr := net.DialUDP("udp", nil, &madr)
-	if udpConErr != nil {
-		log.Fatalf("udp connection error %v", udpConErr)
-	}
-	defer udpConn.Close()
-	// Setting Socket options and buffer size
-	setSockOpts(udpConn, ttl, tos)
-	udpConn.SetWriteBuffer(datalen)
-	for {
-		count, err := io.ReadFull(resp.Body, data)
-		if err != nil {
-			fmt.Printf("Error while reading source %v\n", err)
-		}
-		if count < datalen {
-			log.Printf("received smal amount of data %d except %d\n", count, len(data))
-		}
-
-		//udpConn, udpConErr := net.Dial("udp", "239.23.15.102:1234")
-		for i := 0; i+datalen <= count; i += datalen {
-			cnt, err := udpConn.Write(data[i : i+datalen])
-			if err != nil {
-				fmt.Printf("Error while writing to socket %v bytes\n", cnt)
-			}
-		}
-	}
-}
-
-func setSockOpts(udpConn *net.UDPConn, params ...int) {
-	fmt.Printf("Setting up TTL and TOS for outgoing traffic\n")
-	var tos = 136
-	var ttl = 64
-	if len(params) == 2 {
-		ttl = params[0]
-		tos = params[1]
-	}
-	fl, err := udpConn.File()
-	if err != nil {
-		fmt.Printf("Error getting file object %v\n", err)
-	}
-	fd := fl.Fd()
-	sockerr := syscall.SetsockoptInt(int(fd), syscall.SOL_IP, syscall.IP_MULTICAST_TTL, ttl)
-	if sockerr != nil {
-		fmt.Printf("Error setting TTL sockopt %v\n", sockerr)
-	}
-	sockerr = syscall.SetsockoptInt(int(fd), syscall.SOL_IP, syscall.IP_TOS, tos)
-	if sockerr != nil {
-		fmt.Printf("Error setting IP_TOS sockopt %v\n", sockerr)
-	}
+	log.Printf("%s -> %s\n", *source, *udp)
+	readhttp(*source, *udp, *ttl, *tos)
 }
 
 func usage() {
-	fmt.Printf("%v -s <source http address> -d <destination udp address>"+
-		"[-t ttl for multicast in dec] [-o tos in hex] -h help\n", os.Args[0])
+	fmt.Printf("%s -s <source http address> -d <destination multicast address>"+
+		"-h help [-t ttl] [-p tos in dec default 136]\n", os.Args[0])
+	os.Exit(1)
+}
+
+func readhttp(httpaddr string, udpaddr string, ttl int, tos int) {
+	resp, err := http.Get(httpaddr)
+	maddr := net.UDPAddr{IP: net.ParseIP(udpaddr), Port: 1234}
+	udpConn, udpConErr := net.DialUDP("udp", nil, &maddr)
+
+	if err != nil {
+		log.Fatalf("Error while connecting to source %v", err)
+	}
+	defer resp.Body.Close()
+	if udpConErr != nil {
+		log.Fatalf("udp connection error %v", udpConErr)
+	}
+	// Setting TOS and TTL for connection
+	// ipv4conn := ipv4.NewPacketConn(udpConn)
+	// ipv4conn.SetTOS(0xc0)
+	// ipv4conn.SetMulticastTTL(64)
+
+	// Buffer without it error in
+	udpConn.SetWriteBuffer(datalen)
+	defer udpConn.Close()
+	log.Printf("Dialed successful %s\n", httpaddr)
+	udpsockopt(udpConn, ttl, tos)
+	data := make([]byte, bufsize)
+
+	for {
+		count, err := io.ReadFull(resp.Body, data)
+		if err != nil {
+			log.Printf("Error while reading data %v", err)
+			break
+		}
+		for i := 0; i+datalen <= count; i += datalen {
+			udpConn.Write(data[i : i+datalen])
+		}
+	}
+}
+
+func udpsockopt(udpConn *net.UDPConn, ttl int, tos int) {
+	file, err := udpConn.File()
+	fd := file.Fd()
+
+	err = syscall.SetsockoptInt(int(fd), syscall.SOL_IP, syscall.IP_MULTICAST_TTL, ttl)
+	if err != nil {
+		log.Printf("Error setting TTL sockopt %v\n", err)
+	}
+	err = syscall.SetsockoptInt(int(fd), syscall.SOL_IP, syscall.IP_TOS, tos)
+	if err != nil {
+		log.Printf("Error setting TOS sockopt %v", err)
+	}
 }
